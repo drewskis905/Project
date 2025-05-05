0305fb332dae45f5debdeba7db6912632a1f8137
Andrew Pham <andrewpham212@gmail.com>
2025-05-05 00:20:45 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,208 +1,122 @@
-# Author: Andrew Pham and Matthew Richardson
-# Description: Player wonders around a map and tries to get to the exit without dying from monsters.
-# Date: 10/30/24
-import hero
-import enemy
-import map
+# Author: Andrew Pham
+# Description: Programs allows you to add a task, remove, postpone, and see what task you have coming up.
+# Date: 12/03/24
+import tasklist
 import check_input
-import random
+def main_menu():
+    #displays the main menu and returns the user’s valid input
+    user_input = check_input.get_int_range("1. Display all task\n2. Display current task\n3. Add new task\n4. Mark current task complete\n5. Postpone current task\n6. Search tasks by date\n7. Save and quit\nEnter choice: ", 1, 7)
+    print()
+    return user_input
+
+def get_date():
+    #prompt user to enter month, day, and year and then returns it in the correct format
+    month = ''
+    day = ''
+    year = ''
+    print("Enter new due date: ")
+    user_input = check_input.get_int_range("Enter month: ", 1, 12)
+    if user_input < 10:
+        month = '0' + str(user_input)
+    else:
+        month = str(user_input)
+    user_input = check_input.get_int_range("Enter day: ", 1, 31)
+    if user_input < 10:
+        day = '0' + str(user_input)
+    else:
+        day = str(user_input)
+    user_input = check_input.get_int_range("Enter year: ", 2000, 2100)
+    year = str(user_input)
+    return month + "/" + day + "/" + year
+
+def get_time():
+    #prompt user to enter hour and minutes then returns it in the correct format
+    hour = ''
+    minute = ''
+    user_input = check_input.get_int_range("Enter hour: ", 0, 23)
+    if user_input < 10:
+        hour = '0' + str(user_input)
+    else:
+        hour = str(user_input)
+    user_input = check_input.get_int_range("Enter minute: ", 0, 59)
+    if user_input < 10:
+        minute = '0' + str(user_input)
+    else:
+        minute = str(user_input)
+    return hour + ":" + minute
 
 def main():
-    user_name = input("What is your name, traveler? ")
-    h = hero.Hero(user_name)
-    m = map.Map()
-    m.reveal(h.loc)
+    tlist = tasklist.Tasklist()
     i = True
-    while i: #loop of the game
-        print(h)
-        print(m.show_map(h.loc))
-        user_input = check_input.get_int_range("1. Go North\n2. Go South\n3. Go East\n4. Go West\n5. Quit\nEnter Choice: ", 1, 5)
+    while i:
+        print("-Tasklist-")
+        print("You have " + str(len(tlist)) + " tasks.")
+        user_input = main_menu()
         if user_input == 1:
-            move = h.go_north()
-            if move == 'o': #check if its 'o'
-                print("You cannot go that way...")
+            if len(tlist) == 0:
+                print("All task are completed!\n")
+            else:
+                for count, t in enumerate(tlist, 1):
+                    print(f"{count}. {t}")
                 print()
-            
-            elif m[move[0]][move[1]] == 'n': #checks if 'n
-                m.reveal(move)
-                print("There is nothing here...\n")
-                m.remove_at_loc(move)
-            
-            elif m[move[0]][move[1]] == 's': #checks if 's'
-                print("You somehow made it back to the start...\n")
-            
-            elif m[move[0]][move[1]] == 'i': #checks if 'i'
-                m.reveal(move)
-                print("You found a Health Potion! You drank it to restore your health.\n")
-                h.heal()
-                m.remove_at_loc(move)
-            
-            elif m[move[0]][move[1]] == 'm': #checks if 'm'
-                monster = enemy.Enemy()
-                print(f"You have encounter a {monster}")
-                j = True
-                while j: #loop for user to attack the monster or run away(exits when monster is killed or user runs away)
-                    user_action = check_input.get_int_range(f"1. Attack {monster.name}\n2. Run Away\nEnter choice: ", 1, 2)
-                    if user_action == 1: #user attacks
-                        result = h.attack(monster)
-                        print(result)
-                        if monster.hp != 0: #checks if monster is not already dead
-                            result = monster.attack(h)
-                            print(result)
-                            if h.hp == 0: #after monster attacks hero, checks if hero died from that monster and ends the game if hero died
-                                print("You have been slain.\nGAME OVER!")
-                                j = False
-                                i = False
-                        elif monster.hp == 0 :
-                            print(f"You have slain a {monster.name}\n")
-                            j = False
-                    else: #user runs away
-                        m.reveal(move)
-                        rand_move = [h.go_north(), h.go_south(), h.go_east(), h.go_west()]
-                        move = random.choice(rand_move)
-                        j = False
-
-            elif m[move[0]][move[1]] == 'f':
-                print("Congratulations! You found the exit.\nGAME OVER!")
-                i = False
         
         elif user_input == 2:
-            move = h.go_south()
-            if move == 'o': #check if its 'o'
-                print("You cannot go that way...")
+            if len(tlist) == 0:
+                print("All task are completed!")
+            else:
+                print("Current task is:")
+                print(tlist.get_current_task())
                 print()
-            
-            elif m[move[0]][move[1]] == 'n': #checks if 'n
-                m.reveal(move)
-                print("There is nothing here...\n")
-                m.remove_at_loc(move)
-            
-            elif m[move[0]][move[1]] == 's': #checks if 's'
-                print("You somehow made it back to the start...\n")
-            
-            elif m[move[0]][move[1]] == 'i': #checks if 'i'
-                m.reveal(move)
-                print("You found a Health Potion! You drank it to restore your health.\n")
-                h.heal()
-                m.remove_at_loc(move)
-            
-            elif m[move[0]][move[1]] == 'm': #checks if 'm'
-                monster = enemy.Enemy()
-                print(f"You have encounter a {monster}")
-                j = True
-                while j: #loop for user to attack the monster or run away(exits when monster is killed or user runs away)
-                    user_action = check_input.get_int_range(f"1. Attack {monster.name}\n2. Run Away\nEnter choice: ", 1, 2)
-                    if user_action == 1: #user attacks
-                        result = h.attack(monster)
-                        print(result)
-                        if monster.hp != 0: #checks if monster is not already dead
-                            result = monster.attack(h)
-                            print(result)
-                        elif monster.hp == 0 :
-                            print(f"You have slain a {monster.name}\n")
-                            j = False
-                    else: #user runs away
-                        m.reveal(move)
-                        rand_move = [h.go_north(), h.go_south(), h.go_east(), h.go_west()]
-                        move = random.choice(rand_move)
-                        j = False
-
-            elif m[move[0]][move[1]] == 'f':
-                print("Congratulations! You found the exit.\nGAME OVER!")
-                i = False
         
         elif user_input == 3:
-            move = h.go_east()
-            if move == 'o': #check if its 'o'
-                print("You cannot go that way...")
-                print()
-            
-            elif m[move[0]][move[1]] == 'n': #checks if 'n
-                m.reveal(move)
-                print("There is nothing here...\n")
-                m.remove_at_loc(move)
-            
-            elif m[move[0]][move[1]] == 's': #checks if 's'
-                print("You somehow made it back to the start...\n")
-            
-            elif m[move[0]][move[1]] == 'i': #checks if 'i'
-                m.reveal(move)
-                print("You found a Health Potion! You drank it to restore your health.\n")
-                h.heal()
-                m.remove_at_loc(move)
-            
-            elif m[move[0]][move[1]] == 'm': #checks if 'm'
-                monster = enemy.Enemy()
-                print(f"You have encounter a {monster}")
-                j = True
-                while j: #loop for user to attack the monster or run away(exits when monster is killed or user runs away)
-                    user_action = check_input.get_int_range(f"1. Attack {monster.name}\n2. Run Away\nEnter choice: ", 1, 2)
-                    if user_action == 1: #user attacks
-                        result = h.attack(monster)
-                        print(result)
-                        if monster.hp != 0: #checks if monster is not already dead
-                            result = monster.attack(h)
-                            print(result)
-                        elif monster.hp == 0 :
-                            print(f"You have slain a {monster.name}\n")
-                            j = False
-                    else: #user runs away
-                        m.reveal(move)
-                        rand_move = [h.go_north(), h.go_south(), h.go_east(), h.go_west()]
-                        move = random.choice(rand_move)
-                        j = False
-
-            elif m[move[0]][move[1]] == 'f':
-                print("Congratulations! You found the exit.\nGAME OVER!")
-                i = False
-
+            task_description = input("Enter a task: ")
+            print("Enter due date: ")
+            due_month = check_input.get_int_range("Enter month: ", 1, 12)
+            due_day = check_input.get_int_range("Enter day: ", 1, 31)
+            due_year = check_input.get_int_range("Enter year: ", 2000, 2100)
+            print("Enter time: ")
+            due_hour = check_input.get_int_range("Enter hour: ", 0, 23)
+            due_minute = check_input.get_int_range("Enter minute: ", 0, 59)
+            date = str(due_month) + "/" + str(due_day) + "/" + str(due_year)
+            time = str(due_hour) + ":" + str(due_minute)
+            tlist.add_task(task_description, date, time)
+        
+        
         elif user_input == 4:
-            move = h.go_west()
-            if move == 'o': #check if its 'o'
-                print("You cannot go that way...")
+            if len(tlist) == 0:
+                print("All task are completed!")
+            else:
+                print(f"Marking current task complete:\n{tlist.get_current_task()}")
+                tlist.mark_complete()
+                print(f"New current task is:\n{tlist.get_current_task()}\n")
+        
+        elif user_input == 5:
+            if len(tlist) == 0:
+                print("All task are completed!")
+            else:
+                print(f"Postponing task:\n{tlist.get_current_task()}\n")
+                tlist.postpone_task(get_date(), get_time())
                 print()
-            
-            elif m[move[0]][move[1]] == 'n': #checks if 'n'
-                m.reveal(move)
-                print("There is nothing here...\n")
-                m.remove_at_loc(move)
-            
-            elif m[move[0]][move[1]] == 's': #checks if 's'
-                print("You somehow made it back to the start...\n")
-            
-            elif m[move[0]][move[1]] == 'i': #checks if 'i'
-                m.reveal(move)
-                print("You found a Health Potion! You drank it to restore your health.\n")
-                h.heal()
-                m.remove_at_loc(move)
-            
-            elif m[move[0]][move[1]] == 'm': #checks if 'm'
-                monster = enemy.Enemy()
-                print(f"You have encounter a {monster}")
-                j = True
-                while j: #loop for user to attack the monster or run away(exits when monster is killed or user runs away)
-                    user_action = check_input.get_int_range(f"1. Attack {monster.name}\n2. Run Away\nEnter choice: ", 1, 2)
-                    if user_action == 1: #user attacks
-                        result = h.attack(monster)
-                        print(result)
-                        if monster.hp != 0: #checks if monster is not already dead
-                            result = monster.attack(h)
-                            print(result)
-                        elif monster.hp == 0 :
-                            print(f"You have slain a {monster.name}\n")
-                            j = False
-                    else: #user runs away
-                        m.reveal(move)
-                        rand_move = [h.go_north(), h.go_south(), h.go_east(), h.go_west()]
-                        move = random.choice(rand_move)
-                        print("You ran away!")
-                        j = False
 
-            elif m[move[0]][move[1]] == 'f':
-                print("Congratulations! You found the exit.\nGAME OVER!")
-                i = False
-
-        else:
+        elif user_input == 6:
+            if len(tlist) == 0:
+                print("All task are completed!")
+            else:
+                print("Search by Date:")
+                s_month = check_input.get_int_range("Enter month: ", 1, 12)
+                s_day = check_input.get_int_range("Enter day: ", 1, 31)
+                s_year = check_input.get_int_range("Enter year: ", 2000, 2100)
+                s_date = str(s_month) + "/" + str(s_day) + "/" + str(s_year)
+                for count, t in enumerate(tlist, 1):
+                    if t.date == s_date:
+                        print(f"{count}. {t.description}")
+                    else:
+                        print("Cannot find task with that date.")
+                print()
+        
+        
+        elif user_input == 7:
+            tlist.save_file()
+            print("Saving list...")
             i = False
-
 main()
